938f48fde06258d84480149b0331e064a72be310
mannnrachman <muhammad_rachman@hotmail.com>
2026-05-10 12:42:32 +0800

    chore: restructure project and cleanup old static files

--- routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateChatResponse } from '../controllers/chatController.js';
-
-const router = express.Router();
-
-router.post('/', generateChatResponse);
-
-export default router;
